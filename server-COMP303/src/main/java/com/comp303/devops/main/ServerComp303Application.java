c8fe61c80123582a3df57ef11c5c9147055c3f27
longquan98@gmail.com <longquan98@gmail.com>
2024-03-29 01:39:05 -0400

    Create Spring Boot server

---
+++ server-COMP303/src/main/java/com/comp303/devops/main/ServerComp303Application.java
@@ -0,0 +1,13 @@
+package com.comp303.devops.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerComp303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerComp303Application.class, args);
+	}
+
+}
---
+++ server-COMP303/src/test/java/com/comp303/devops/main/ServerComp303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp303.devops.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerComp303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
