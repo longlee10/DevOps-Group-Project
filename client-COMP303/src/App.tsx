fd096265231b5d0b5283000c85607aa27c2c8fb1
longquan98@gmail.com <longquan98@gmail.com>
2024-04-10 11:27:22 -0400

    Fixing routing bug

--- client-COMP303/src/App.tsx
+++ client-COMP303/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Book from "./components/Book";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* All other routes */}
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/book" element={<Book />} />
       </Routes>
     </Router>
