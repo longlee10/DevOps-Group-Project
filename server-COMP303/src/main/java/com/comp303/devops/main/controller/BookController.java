1e3db01d099fd29470ccd66396944d67d21e9f7d
longquan98@gmail.com <longquan98@gmail.com>
2024-04-10 10:32:15 -0400

    Enable CORS for Book controller

--- server-COMP303/src/main/java/com/comp303/devops/main/controller/BookController.java
+++ server-COMP303/src/main/java/com/comp303/devops/main/controller/BookController.java
@@ -1,10 +1,9 @@
 package com.comp303.devops.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,29 +25,34 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
+	@CrossOrigin
 	@GetMapping
 	public Flux<Book> getAll() {
 		System.out.println("All the employee information");
 		return bookService.getAll();
 	}
 	
+	@CrossOrigin
 	@GetMapping("{id}")
 	public Mono<Book> getById(@PathVariable("id") final String id) {
 		System.out.println("One employee information based for the given ID");
 		return bookService.getById(id);
 	}
 	
+	@CrossOrigin
 	@PostMapping
 	public Mono<Book> save(@RequestBody final Book book) {
 		return bookService.save(book);
 				
 	}
 	
+	@CrossOrigin
 	@PutMapping("{id}")
 	public Mono<Book> updateById(@PathVariable("id") final String id, @RequestBody final Book book) {
 		return bookService.update(id, book);
 	}
 	
+	@CrossOrigin
 	@DeleteMapping("{id}")
 	public Mono<Book> delete(@PathVariable final String id) {
 		return bookService.delete(id);
